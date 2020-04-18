aaf528c2fa9b7f87be3dc97138cafc4095e92cbf
cloudmelon <urinaway@gmail.com>
2020-04-18 18:58:18 +0100

    add python code call text analytics cog service

---
+++ CallTextAnalyticsAPI/call_api.py
@@ -0,0 +1,38 @@
+import os , json
+from azure.cognitiveservices.language.textanalytics import TextAnalyticsClient
+from msrest.authentication import CognitiveServicesCredentials
+
+key = "<your subscription key >"
+endpoint = "< endpoint of your text analytics cog service"
+
+def authenticateClient():
+    credentials = CognitiveServicesCredentials(key)
+    text_analytics_client = TextAnalyticsClient(
+        endpoint=endpoint, credentials=credentials)
+    return text_analytics_client
+
+def sentiment():
+    
+    client = authenticateClient()
+
+    try:
+        documents = [
+            {"id": "1", "language": "en", "text": "I had the best day of my life."},
+            {"id": "2", "language": "en",
+                "text": "This was a waste of my time. The speaker put me to sleep."},
+            {"id": "3", "language": "es", "text": "No tengo dinero ni nada que dar..."},
+            {"id": "4", "language": "it",
+                "text": "L'hotel veneziano era meraviglioso. È un bellissimo pezzo di architettura."}
+        ]
+
+        response = client.sentiment(documents=documents)
+        for document in response.documents:
+            print("Document Id: ", document.id, ", Sentiment Score: ",
+                  "{:.2f}".format(document.score))
+
+    except Exception as err:
+        print("Encountered exception. {}".format(err))
+
+
+sentiment()
+
